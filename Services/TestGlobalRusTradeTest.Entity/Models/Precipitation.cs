b21ae70902f5d7279cfa81588c152ce22ad99ed8
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-01-21 13:08:19 +0300

    Создание отдельной библиотеки и в ней создание сущности прогноза погоды

---
+++ Services/TestGlobalRusTradeTest.Entity/Models/Precipitation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestGlobalRusTradeTest.Entity.Models
+{
+    public class Precipitation
+    {
+        private string _Precipitation;
+        private string[] _precipitation = { "солнечно", "облачно", "небольшой дождь", "сильный дождь", "гроза" };
+        public Precipitation(int i)
+        {
+            _Precipitation = _precipitation[i];
+        }
+
+        public override string ToString()
+        {
+            return _Precipitation;
+        }
+    }
+}
---
+++ Services/TestGlobalRusTradeTest.Entity/Models/Wind.cs
@@ -0,0 +1,18 @@
+﻿namespace TestGlobalRusTradeTest.Entity.Models
+{
+    public class Wind
+    {
+        public Wind(int i, int b)
+        {
+            _wind = i;
+            _Direction = _direction[b];
+        }
+        private int _wind { get; set; }
+
+        private string _Direction { get; set; }
+
+        private string[] _direction = { "С", "СВ", "В", "ЮВ", "Ю", "ЮЗ", "З", "СЗ" };
+
+        public override string ToString() => $"{_wind}=>{_Direction}";
+    }
+}
